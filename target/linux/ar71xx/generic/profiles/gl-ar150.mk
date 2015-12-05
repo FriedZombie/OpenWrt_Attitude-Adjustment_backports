7d6cefa1d4e70d4a29950a8e08ed507b83a7e935
Matthias Strubel <matthias.strubel@aod-rpg.de>
2015-12-05 11:13:10 +0100

    Device backport Gl-iNet AR-150
      Closes #6
      Based upon quilt patch series:
        https://github.com/domino-team/OpenWrt-patches/tree/master/AR150-AA1209
    
      Manually applied, because patch series did not apply on this modified
      repository.

---
+++ target/linux/ar71xx/generic/profiles/gl-ar150.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+define Profile/GL_AR150
+	NAME:=GL-AR150
+	PACKAGES:=kmod-usb-core kmod-usb2
+endef
+
+define Profile/GL_AR150/Description
+	Configuration of GL-AR150, Wifi for everything.
+endef
+
+$(eval $(call Profile,GL_AR150))
